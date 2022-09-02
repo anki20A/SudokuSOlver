abd40c02701c4e42e75c50a3bbb03f5da70ad62e
anki20A <110227744+anki20A@users.noreply.github.com>
2022-09-03 02:01:39 +0530

    Create sudokuMain.java

---
+++ sudokuMain.java
@@ -0,0 +1,10 @@
+package sudoku;
+
+
+import java.io.FileNotFoundException;
+public class sudokuMain {
+	 public static void main(String[] args) throws FileNotFoundException {
+		 GUI gui = new GUI();
+			gui.createGUI();
+	 }
+}
